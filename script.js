657e54d848ef9155ae5f964f53b5c6cdb88feb95
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-15 10:24:06 +0700

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+// ex2.1
+const checkEven = function (num) {
+	return num % 2 === 0 ? "even" : "odd";
+};
+console.log(checkEven(2));
+
+//ex2.2
+const checkEvenarrow =  num => num % 2 === 0 ? "even" : "odd";
+console.log(checkEvenarrow(4));
